d4e27fc19819e5c9e6093ce119a281dba51b9e5f
tatsuya.nagai <nagaitatsuya98@gmail.com>
2021-05-05 09:25:00 +0000

    update group/:token page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,7 @@ class ApplicationController < ActionController::Base
   
   def require_user_logged_in
     unless logged_in?
+      flash[:danger] = 'ログインが必要です'
       redirect_to login_url
     end
   end
