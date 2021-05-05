cb9c96c1141c652809e020a0bf6eda9eaf403886
tatsuya.nagai <nagaitatsuya98@gmail.com>
2021-05-05 09:06:52 +0000

    update image display

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :correct_user, only: [:edit, :update]
+  before_action :correct_user, except: [:index, :show, :new, :create]
   
   def index
     search_key = params[:q]
